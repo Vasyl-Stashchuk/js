1d1263bbf65ef3ee56db60610643185be79a5446
Vasyl-Stashchuk <65713313+Vasyl-Stashchuk@users.noreply.github.com>
2021-10-18 06:15:44 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+ var answer =  goals (43, 10, 5);
+ 
+ https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+ 
+ function greet (name, owner) {
+  // Add code here
+  if (name == "Daniel" && owner == "Daniel") {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
+    
+}
